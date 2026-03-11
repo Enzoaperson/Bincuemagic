b5b42e9ee119c6a08c50e9d738bda8f7fc4ae1e5
Enzo <enzomlg88@gmail.com>
2026-03-11 08:13:41 -0700

    Fixed the wave header creator so it actually works with songs longer than 5:14

--- src/songextract.c
+++ src/songextract.c
@@ -4,9 +4,11 @@
 extern char DEBUG;
 extern char EXPORT_TO_WAVE;
 
-char wave_header[] = {0x52, 0x49, 0x46, 0x46, 0x44, 0x9D, 0x4E, 0x03, 0x57, 0x41, 0x56, 0x45, 0x66, 0x6D, 0x74, 0x20, 0x10, 0x00, 0x00, 
+char wave_header[] = {0x52, 0x49, 0x46, 0x46, //File Size is slotted here
+       	0x57, 0x41, 0x56, 0x45, 0x66, 0x6D, 0x74, 0x20, 0x10, 0x00, 0x00, 
 	0x00, 0x01, 0x00, 0x02, 0x00, 0x44, 0xAC, 0x00, 0x00, 0x10, 0xB1, 0x02, 0x00, 0x04, 0x00, 0x10, 0x00, 0x64, 0x61, 0x74,
-        0x61, 0x20, 0x9D, 0x4E, 0x03};
+        0x61 //Data Size is slotted here at end
+};
 
 int songextract(char *bin_file_name, int starting_bytes, int length_bytes, char *output_file_name){
 	if(DEBUG){printf("Starting Input: %d,%d\n", starting_bytes, length_bytes);}
@@ -30,12 +32,30 @@ int songextract(char *bin_file_name, int starting_bytes, int length_bytes, char 
 	}
 	if(DEBUG){printf("Fixed Input: %d,%d\n", starting_bytes, length_bytes);}
 	fseek(bin_file, starting_bytes, SEEK_SET); //Getting into position
+	//Add wave header if requested
 	if(EXPORT_TO_WAVE){
 		if(DEBUG){printf("Adding WAV header\n");}
-		for(int i = 0; i < 0x2C; i++){
+		//first 4 bits
+		for(char i = 0; i <= 3; i++){
 			fputc(wave_header[i], output_file);
 		}
+		//length of file
+		//Ordered from lowest byte to highest
+		fputc(((length_bytes + 0x24) & 0xFF), output_file);
+		fputc((((length_bytes + 0x24) >> 8) & 0xFF), output_file);
+		fputc((((length_bytes + 0x24) >> 16) & 0xFF), output_file);
+		fputc((((length_bytes + 0x24) >> 24) & 0xFF), output_file);
+		for(char i = 4; i < 36; i++){
+			fputc(wave_header[i], output_file);
+		}
+		//length of song
+		fputc(((length_bytes) & 0xFF), output_file);
+		fputc((((length_bytes) >> 8) & 0xFF), output_file);
+		fputc((((length_bytes) >> 16) & 0xFF), output_file);
+		fputc((((length_bytes) >> 24) & 0xFF), output_file);
+
 	}
+	//Write bits to file
 	for(int i = 0; i < length_bytes; i++){
 		fputc(fgetc(bin_file), output_file);
 	}
